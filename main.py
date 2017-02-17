3949982248c07ba71d9de4fd2817beb0e8c63524
numbe9988 <matteobit@hotmail.com>
2017-02-17 20:38:26 +0100

    a

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import primeGen
 
+#prime = primeGen.primeGen()
+
+#print(prime.calcPrim(400))
+
+f= open("prova.txt", "w")
 prime = primeGen.primeGen()
 
-print(prime.calcPrim(400))
+
+for i in range(1,10000000):
+    if i%10000 == 0:
+        print(i)
+
+    if prime.isPrime(i):
+        f.write(str(i) + " ")
--- textNum.py
+++ textNum.py
@@ -7,27 +7,28 @@
 
 
 
-text = "ciao"
+text = "abc"
 
 
 class selfNum():
-    Z = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+    Z = ["","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
          "Z", " "]
 
     def textToNum(self, text, Z_VAL=27):
         n = 0
         for i in text:
             print(i.upper())
-            n = (text.index(i) + self.Z.index(i.upper())) * Z_VAL
+            n = (text.index(i)+ 1 + self.Z.index(i.upper())) * Z_VAL
+            print(self.Z.index(i.upper()))
 
         return n
 
     def numToText(self, num, Z_VAL=27):
         res = ""
         while num != 0:
-            res += Z[num%Z_VAL]
+            res += self.Z[num%Z_VAL]
             num = (num-(num%Z_VAL))/Z_VAL
+        return res
 
 
 print(selfNum().textToNum(text))
-print(selfNum().numToText(selfNum().textToNum(text)))
