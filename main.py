5161bb6fd5c68a11eb197ced5140c5745135d14d
Unknown <matteobit@hotmail.com>
2017-02-23 10:24:01 +0100

    text num ok

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 import primeGen
 
-#prime = primeGen.primeGen()
-
-#print(prime.calcPrim(400))
-
-f= open("prova.txt", "w")
 prime = primeGen.primeGen()
 
-
-for i in range(1,10000000):
-    if i%10000 == 0:
-        print(i)
-
-    if prime.isPrime(i):
-        f.write(str(i) + " ")
+print(prime.calcPrim(1024))
--- textNum.py
+++ textNum.py
@@ -5,30 +5,31 @@
 # r = val mod 27 = letter
 # val - r/27 =
 
-
-
 text = "abc"
 
-
 class selfNum():
-    Z = ["","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-         "Z", " "]
-
-    def textToNum(self, text, Z_VAL=27):
+    def text2num(self, text):  # Testo a numero in Z27
         n = 0
+        l = list()
         for i in text:
-            print(i.upper())
-            n = (text.index(i)+ 1 + self.Z.index(i.upper())) * Z_VAL
-            print(self.Z.index(i.upper()))
-
+            l.append(ord(i))
+        for i in l:
+            n = n * 400 + i
         return n
 
-    def numToText(self, num, Z_VAL=27):
-        res = ""
+    def num2text(self, num):  # Numero a testo in Z27
+        l = list()
+        res = num % 400
+        l.append(res)
         while num != 0:
-            res += self.Z[num%Z_VAL]
-            num = (num-(num%Z_VAL))/Z_VAL
-        return res
+            num, res = self._calculation(num, res, 400)
+            l.append(res)
+        l.remove(0)
+        return "".join([chr(i) for i in reversed(l)])
 
+    def _calculation(self, t, k, m):  # Calcoli
+        n = (t - k) // m
+        q = n % m
+        return n, q
 
-print(selfNum().textToNum(text))
+print(selfNum().num2text(selfNum().text2num(text)))
