ff05d26a8ce48d44a906e704498e65278ead5a0c
Unknown <matteobit@hotmail.com>
2017-05-24 12:34:51 +0200

    altop

--- mainPrimeFile.py
+++ mainPrimeFile.py
@@ -4,9 +4,9 @@ import sys
 
 args = sys.argv
 
-NOME_FILE = args[1]
-FROM = int(args[2])
-TO = int(args[3])
+NOME_FILE = "primes/gen1.txt"#args[1]
+FROM = 100000000#int(args[2])
+TO = 200000000#int(args[3])
 
 f = open(NOME_FILE, "r")
 fileBuff = f.read()
@@ -20,7 +20,7 @@ cont = 0
 
 for i in range(FROM, TO):
     cont += 1
-    if cont % 50000 == 0:
+    if cont % 10000 == 0:
         t = time.time() -t
         print(str(i) + " Tempo: " + str(t))
         t = time.time()
